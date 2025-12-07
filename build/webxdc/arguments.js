346f19d53f6073a2b4f73d7b0be1da2236d53d57
adbenitez <asieldbenitez@gmail.com>
2025-12-07 03:44:37 +0100

    add comment to build/webxdc/arguments.js

--- build/webxdc/arguments.js
+++ build/webxdc/arguments.js
@@ -1,1 +1,11 @@
+/**
+   To package a cartridge as a standalone webxdc game:
+   1. rename tic80.xdc to tic80.zip to handle it as zip file if necessary
+   2. modify manifest.toml, set your game name and url
+   3. modify icon.png with your game's icon
+   4. add your cartridge as "cart.tic" file to the tic80.zip
+   5. uncomment the code below so your cart.tic file gets loaded
+   6. rename tic80.zip to your-game.xdc
+   7. send in chats!
+ */
 //var ARGUMENTS = ['cart.tic'];
